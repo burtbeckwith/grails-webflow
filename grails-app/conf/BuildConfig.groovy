594a93b1f49b1d748ccdfa8647afa0856ba9d03b
graeme <graeme@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2009-12-03 16:04:05 +0000

    1.2 RC1 version of the webflow plugin
    git-svn-id: https://svn.codehaus.org/grails/trunk/grails-plugins/grails-webflow/trunk@9203 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -9,7 +9,7 @@ grails.project.dependency.resolution = {
 	}
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
-        compile 'org.grails:grails-webflow:1.2.0.BUILD-SNAPSHOT',
+        compile 'org.grails:grails-webflow:1.2.0.RC1',
  				'org.springframework.webflow:org.springframework.webflow:2.0.8.RELEASE',
  				'org.springframework.webflow:org.springframework.binding:2.0.8.RELEASE',
  				'org.springframework.webflow:org.springframework.js:2.0.8.RELEASE'
