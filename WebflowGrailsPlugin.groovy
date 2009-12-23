83aa98408b0aae013ece50d11446ceeea8b11d31
brownj <brownj@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2009-12-23 18:05:32 +0000

    set version 1.2.1.SNAPSHOT
    git-svn-id: https://svn.codehaus.org/grails/trunk/grails-plugins/grails-webflow/trunk@9259 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

--- WebflowGrailsPlugin.groovy
+++ WebflowGrailsPlugin.groovy
@@ -1,7 +1,7 @@
 import org.codehaus.groovy.grails.webflow.WebFlowPluginSupport
 
 class WebflowGrailsPlugin {
-    def version = "1.2.0"
+    def version = "1.2.1.SNAPSHOT"
     def dependsOn = [core:"1.2 > *",i18n:"1.2 > *", controllers:"1.2 > *"]
     def observe = ['controllers']
     def loadAfter = ['hibernate']
