ea998957087da07f523c3d18c346f578b1a7091a
Graeme Rocher <graeme.rocher@gmail.com>
2010-09-29 11:54:40 +0200

    1.3.5 version change

--- WebflowGrailsPlugin.groovy
+++ WebflowGrailsPlugin.groovy
@@ -1,7 +1,7 @@
 import org.codehaus.groovy.grails.webflow.WebFlowPluginSupport
 
 class WebflowGrailsPlugin {
-    def version = "1.3.5.BUILD-SNAPSHOT"
+    def version = "1.3.5"
     def dependsOn = [core:"1.3 > *",i18n:"1.3 > *", controllers:"1.3 > *"]
     def observe = ['controllers']
     def loadAfter = ['hibernate']
