28911a4cb6af573641ae83126f0109840c9e91fd
Burt Beckwith <beckwithb@vmware.com>
2011-12-21 22:13:12 -0500

    cleanup

--- WebflowGrailsPlugin.groovy
+++ WebflowGrailsPlugin.groovy
@@ -2,25 +2,17 @@ import org.codehaus.groovy.grails.webflow.WebFlowPluginSupport
 
 class WebflowGrailsPlugin {
     def version = "2.1.0.BUILD-SNAPSHOT"
-    def dependsOn = [core:"2.1 > *",i18n:"2.1 > *", controllers:"2.1 > *"]
+    def dependsOn = [core:"2.1 > *",
+                     i18n:"2.1 > *",
+                     controllers:"2.1 > *"]
     def observe = ['controllers']
     def loadAfter = ['hibernate']
 
-    // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.1 > *"
-    // resources that are excluded from plugin packaging
-    def pluginExcludes = [
-            "grails-app/views/error.gsp"
-    ]
-
     def author = "Graeme Rocher"
     def authorEmail = "graeme.rocher@springsource.com"
     def title = "Spring Web Flow Plugin"
-    def description = '''\
-Integrates Spring Web Flow with Grails
-'''
-
-    // URL to the plugin's documentation
+    def description = 'Integrates Spring Web Flow with Grails'
     def documentation = "http://grails.org/plugin/webflow"
 
     def doWithSpring = WebFlowPluginSupport.doWithSpring
@@ -30,5 +22,4 @@ Integrates Spring Web Flow with Grails
     def doWithApplicationContext = WebFlowPluginSupport.doWithApplicationContext
 
     def onChange = WebFlowPluginSupport.onChange
-
 }
--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,35 +1,34 @@
-grails.project.class.dir = "target/classes"
-grails.project.test.class.dir = "target/test-classes"
-grails.project.test.reports.dir	= "target/test-reports"
-grails.project.dependency.resolution = {
-    inherits "global" // inherit Grails' default dependencies
-    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
-	repositories {
-		grailsHome()
-		grailsCentral()
-	}
-    dependencies {
-        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
-        compile( "org.grails:grails-webflow:${grailsVersion}",
- 				'org.springframework.webflow:spring-webflow:2.0.8.RELEASE',
- 				'org.springframework.webflow:spring-binding:2.0.8.RELEASE',
- 				'org.springframework.webflow:spring-js:2.0.8.RELEASE' ) {
-			exclude group:"org.springframework", name:"spring-beans"
-			exclude group:"org.springframework", name:"spring-context"
-			exclude group:"org.springframework", name:"spring-core"						
-			exclude group:"org.springframework", name:"spring-web"
-			exclude group:"org.springframework", name:"spring-webmvc"
-			exclude group:"commons-logging", name:"commons-logging"												
-			transitive = false
-		}
-		runtime 'ognl:ognl:2.7.3'
+grails.project.work.dir = 'target'
 
+grails.project.dependency.resolution = {
+
+    inherits "global"
+    log "warn"
+
+    repositories {
+        grailsHome()
+        grailsCentral()
     }
 
-	plugins {
-		build( ":release:1.0.0.RC3" ) {
-			export = false
-		}
-	}
+    dependencies {
+        compile("org.grails:grails-webflow:${grailsVersion}",
+                'org.springframework.webflow:spring-webflow:2.0.8.RELEASE',
+                'org.springframework.webflow:spring-binding:2.0.8.RELEASE',
+                'org.springframework.webflow:spring-js:2.0.8.RELEASE') {
+            exclude group:"org.springframework", name:"spring-beans"
+            exclude group:"org.springframework", name:"spring-context"
+            exclude group:"org.springframework", name:"spring-core"
+            exclude group:"org.springframework", name:"spring-web"
+            exclude group:"org.springframework", name:"spring-webmvc"
+            exclude group:"commons-logging", name:"commons-logging"
+            transitive = false
+        }
+        runtime 'ognl:ognl:2.7.3'
+    }
 
+    plugins {
+        build( ":release:1.0.0" ) {
+            export = false
+        }
+    }
 }
