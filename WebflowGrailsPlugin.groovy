8fd0f1113c821157031ba78719fae3d2ede5a8e1
Jeff Brown <jeff@jeffandbetsy.net>
2012-05-09 15:32:32 -0500

    2.2.0.BUILD-SNAPSHOT

--- WebflowGrailsPlugin.groovy
+++ WebflowGrailsPlugin.groovy
@@ -1,14 +1,14 @@
 import org.codehaus.groovy.grails.webflow.WebFlowPluginSupport
 
 class WebflowGrailsPlugin {
-    def version = "2.1.0.BUILD-SNAPSHOT"
-    def dependsOn = [core:"2.1 > *",
-                     i18n:"2.1 > *",
-                     controllers:"2.1 > *"]
+    def version = "2.2.0.BUILD-SNAPSHOT"
+    def dependsOn = [core:"2.2 > *",
+                     i18n:"2.2 > *",
+                     controllers:"2.2 > *"]
     def observe = ['controllers']
     def loadAfter = ['hibernate']
 
-    def grailsVersion = "2.1 > *"
+    def grailsVersion = "2.2 > *"
     def author = "Graeme Rocher"
     def authorEmail = "graeme.rocher@springsource.com"
     def title = "Spring Web Flow Plugin"
